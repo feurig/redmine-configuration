e24954354ae6b478227573f5518e56703fb5d7ae
D Delmar Davis <don@suspectdevices.com>
2020-10-19 10:36:10 -0700

    kludge to scrape bitbucket

--- var/www/bin/clone-repos.py
+++ var/www/bin/clone-repos.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 from github import Github
+import json
 import subprocess
+import urllib2
+
 username = "feurig"
 repodir = "/var/www/git"
 g = Github()
@@ -9,3 +12,17 @@ for repo in user.get_repos():
     url="git@github.com:"+username+"/"+repo.name+".git"
     print "Cloning :",url
     subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+
+#response = urllib2.urlopen("https://api.bitbucket.org/2.0/repositories/"+username+"?pagelen=100")
+#data=json.loads(response.read())['values']
+#for record in data:
+#    url="git@bitbucket.org:"+username+"/"+record['slug']+".git"
+#    print "Cloning :",url
+#    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+repos=['ems-light','musicbox','straight-from-hell.com','trashterm']
+for repo in repos:
+    url="git@bitbucket.org:"+username+"/"+repo+".git"
+    print "Cloning :",url
+    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+
+
