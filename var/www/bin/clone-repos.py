3e909bab05d6ead4db80193e5f2d0cada304a5f4
D Delmar Davis <don@suspectdevices.com>
2020-10-23 10:42:50 -0700

    add bitbucket repos to update scripts

--- var/www/bin/clone-repos.py
+++ var/www/bin/clone-repos.py
@@ -6,12 +6,12 @@ import urllib2
 
 username = "feurig"
 repodir = "/var/www/git"
-g = Github()
-user = g.get_user(username)
-for repo in user.get_repos():
-    url="git@github.com:"+username+"/"+repo.name+".git"
-    print "Cloning :",url
-    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+#g = Github()
+#user = g.get_user(username)
+#for repo in user.get_repos():
+#    url="git@github.com:"+username+"/"+repo.name+".git"
+#    print "Cloning :",url
+#    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
 
 #response = urllib2.urlopen("https://api.bitbucket.org/2.0/repositories/"+username+"?pagelen=100")
 #data=json.loads(response.read())['values']
@@ -19,10 +19,14 @@ for repo in user.get_repos():
 #    url="git@bitbucket.org:"+username+"/"+record['slug']+".git"
 #    print "Cloning :",url
 #    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
-repos=['ems-light','musicbox','straight-from-hell.com','trashterm']
+#git clone git@bitbucket.org:feurig/missinglink-hardware.git
+repos=['feurig/ems-light','feurig/musicbox','feurig/straight-from-hell.com',
+       'feurig/trashterm','feurig/missinglink-hardware','feurig/bnc-proprietary',
+       'feurig/ems-firmware','feurig/ems-golang','feurig/ems-framework','joedumoulin/sesh']
 for repo in repos:
-    url="git@bitbucket.org:"+username+"/"+repo+".git"
+    url="git@bitbucket.org:"+repo+".git"
     print "Cloning :",url
     subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+
 
 
--- var/www/bin/update-repos.py
+++ var/www/bin/update-repos.py
@@ -8,4 +8,14 @@ user = g.get_user(username)
 for repo in user.get_repos():
     d=repodir+repo.name+".git"
     print "updating :",d
+
+repos=['feurig/ems-light','feurig/musicbox','feurig/straight-from-hell.com',
+       'feurig/trashterm','feurig/missinglink-hardware','feurig/bnc-proprietary',
+       'feurig/ems-firmware','feurig/ems-golang','feurig/ems-framework','joedumoulin/sesh']
+for repo in repos:
+    d=repodir+repo.split('/')[1]+".git"
+    print "updating :",d
     subprocess.call(["/usr/bin/git", "fetch", "-q", "--all", "-p"], cwd=d)
+
+
+
