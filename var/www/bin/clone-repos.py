5e54879c8a4fdeb08ef0d38d8b06376377f4ff4f
D Delmar Davis <don@suspectdevices.com>
2020-10-23 14:01:09 -0700

    cleanup repo clone/update scripts

--- var/www/bin/clone-repos.py
+++ var/www/bin/clone-repos.py
@@ -6,13 +6,31 @@ import urllib2
 
 username = "feurig"
 repodir = "/var/www/git"
-#g = Github()
-#user = g.get_user(username)
-#for repo in user.get_repos():
-#    url="git@github.com:"+username+"/"+repo.name+".git"
-#    print "Cloning :",url
-#    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
 
+githubrepos=['feurig/arduino-core-105','feurig/Arduino_STM32_MIDI_project',
+    'feurig/Cohen','feurig/ems-light','feurig/ems2','feurig/failandflail',
+    'feurig/libmaple','feurig/libmaplemidi-cma','feurig/maple','feurig/maplebacon',
+    'feurig/midimonster2012','feurig/pubcrawler','feurig/python-dialog',
+    'feurig/redmine-configuration','feurig/rtmidi','feurig/Suspect-Devices-Open-Hardware',
+    'feurig/wsgi-bitbucket-mirror',
+]
+
+bitbucketrepos=['feurig/ems-light','feurig/musicbox','feurig/straight-from-hell.com',
+       'feurig/trashterm','feurig/missinglink-hardware','feurig/bnc-proprietary',
+       'feurig/ems-firmware','feurig/ems-golang','feurig/ems-framework','joedumoulin/sesh']
+
+
+for repo in githubrepos:
+    url="git@github.com:"+repo+".git"
+    print "Cloning :",url
+    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+
+for repo in bitbucketrepos:
+    url="git@bitbucket.org:"+repo+".git"
+    print "Cloning :",url
+    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
+
+# used to create lists of repos
 #response = urllib2.urlopen("https://api.bitbucket.org/2.0/repositories/"+username+"?pagelen=100")
 #data=json.loads(response.read())['values']
 #for record in data:
@@ -20,13 +38,11 @@ repodir = "/var/www/git"
 #    print "Cloning :",url
 #    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
 #git clone git@bitbucket.org:feurig/missinglink-hardware.git
-repos=['feurig/ems-light','feurig/musicbox','feurig/straight-from-hell.com',
-       'feurig/trashterm','feurig/missinglink-hardware','feurig/bnc-proprietary',
-       'feurig/ems-firmware','feurig/ems-golang','feurig/ems-framework','joedumoulin/sesh']
-for repo in repos:
-    url="git@bitbucket.org:"+repo+".git"
-    print "Cloning :",url
-    subprocess.call(["/usr/bin/git", "clone", "--mirror", url], cwd=repodir)
 
+
+#g = Github()
+#user = g.get_user(username)
+#for repo in user.get_repos():
+#    print"'"+username+"/"+repo.name+"',"
 
 
--- var/www/bin/update-repos.py
+++ var/www/bin/update-repos.py
@@ -3,19 +3,24 @@ from github import Github
 import subprocess
 username = "feurig"
 repodir = "/var/www/git/"
-g = Github()
-user = g.get_user(username)
-for repo in user.get_repos():
-    d=repodir+repo.name+".git"
-    print "updating :",d
 
-repos=['feurig/ems-light','feurig/musicbox','feurig/straight-from-hell.com',
+githubrepos=['feurig/arduino-core-105','feurig/Arduino_STM32_MIDI_project',
+    'feurig/Cohen','feurig/ems-light','feurig/ems2','feurig/failandflail',
+    'feurig/libmaple','feurig/libmaplemidi-cma','feurig/maple','feurig/maplebacon',
+    'feurig/midimonster2012','feurig/pubcrawler','feurig/python-dialog',
+    'feurig/redmine-configuration','feurig/rtmidi','feurig/Suspect-Devices-Open-Hardware',
+    'feurig/wsgi-bitbucket-mirror',
+]
+
+bitbucketrepos=['feurig/ems-light','feurig/musicbox','feurig/straight-from-hell.com',
        'feurig/trashterm','feurig/missinglink-hardware','feurig/bnc-proprietary',
        'feurig/ems-firmware','feurig/ems-golang','feurig/ems-framework','joedumoulin/sesh']
+
+repos=githubrepos+bitbucketrepos
+
 for repo in repos:
     d=repodir+repo.split('/')[1]+".git"
     print "updating :",d
     subprocess.call(["/usr/bin/git", "fetch", "-q", "--all", "-p"], cwd=d)
-
 
 
